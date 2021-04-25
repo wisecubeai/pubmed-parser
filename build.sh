71ca8c170ab43f56d3582a2b69dedc272fe06f06
Vishnu Vettrivel <vishnu@Vishnus-MacBook-Pro.local>
2021-04-24 22:35:23 -0700

    changing project structure and creating shaded jar

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export ANT_OPTS="-Djavax.xml.accessExternalDTD=true -DenableExternalEntityProcessing=true -Djavax.xml.accessExternalSchema=all"
-ant -verbose
+ant clean javadoc assemble -verbose
 
 
